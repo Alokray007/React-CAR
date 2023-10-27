8577cd8203d2189bce2edd324c9a7974d33b99f1
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-28 03:05:02 +0530

    Background Changer

---
+++ 04-bgchanger/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function App() {
+  const [color, setColor] = useState("red");
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex items-center justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ cursor: "pointer", backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ cursor: "pointer", backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ cursor: "pointer", backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ cursor: "pointer", backgroundColor: "yellow" }}
+          >
+            yellow
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ cursor: "pointer", backgroundColor: "black" }}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
